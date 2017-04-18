a82054bfca41bd0c68138fdc94f4fe513d1aa3f1
meriy100 <ttattataa@gmail.com>
2017-04-18 17:52:04 +0900

    add show and redirects

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,30 @@
 class SubmissionsController < ApplicationController
+  def show
+    find_submission
+  end
+
   def new
     @submission = Submission.new(submitted: Time.zone.now)
+  end
+
+  def create
+    @submission = Submission.new(submission_params)
+    if @submission.save
+      redirect_to @submission
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(
+      :submitted, :file1, :messages, :status, :mark, :comment, :assignment_id, :user_id
+    )
+  end
+
+  def find_submission
+    @submission ||= Submission.find(params[:id])
   end
 end
