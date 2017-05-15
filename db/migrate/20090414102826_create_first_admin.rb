ed5bc69cedb753ae5e000d08a39bdb1b83d57a97
meriy100 <ttattataa@gmail.com>
2017-05-15 18:31:53 +0900

    migration が出来るように

--- db/migrate/20090414102826_create_first_admin.rb
+++ db/migrate/20090414102826_create_first_admin.rb
@@ -1,15 +1,15 @@
 class CreateFirstAdmin < ActiveRecord::Migration
   def self.up
-    down
-    first_admin = User.new()
-    first_admin.login = 'admin'
-    first_admin.email = 'rpcsr_admin@example.org'
-    first_admin.password = 'guessMe'
-    first_admin.password_confirmation = 'guessMe'
-    first_admin.given_name = '-Administrator-'
-    first_admin.family_name = '-Admin-'
-    first_admin.role = 'admin'
-    first_admin.save!
+    # down
+    # first_admin = User.new()
+    # first_admin.login = 'admin'
+    # first_admin.email = 'rpcsr_admin@example.org'
+    # first_admin.password = 'guessMe'
+    # first_admin.password_confirmation = 'guessMe'
+    # first_admin.given_name = '-Administrator-'
+    # first_admin.family_name = '-Admin-'
+    # first_admin.role = 'admin'
+    # first_admin.save!
   end
 
   def self.down
