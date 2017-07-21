566d1eba6a5dca32b96dbcd8117e48a6174a102b
meriy100 <ttattataa@gmail.com>
2017-07-21 13:48:08 +0900

    hwiop

--- lib/generate_json_from_production.rb
+++ lib/generate_json_from_production.rb
@@ -11,6 +11,12 @@ def ids
   }
 end
 
+def ids
+  {
+    632 => "12C1",
+  }
+end
+
 data = Submission.where(assignment_id: ids.keys).includes(:lines).map do |submission|
   { user: submission.user_id, code: ids[submission.assignment_id], time: submission.created_at, status: submission.status, line_count: submission.lines.length }
 end
