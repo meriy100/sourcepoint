08b160f1d833306b674273e237787846b942acf3
meriy100 <ttattataa@gmail.com>
2017-07-21 12:15:31 +0900

    add json.json

--- lib/generate_json_from_production.rb
+++ lib/generate_json_from_production.rb
@@ -1,2 +1,19 @@
-[637, 636, 638, 639, 632, 633, 634, 635]
+def ids
+  {
+   632 => "1401",
+   633 => "1402",
+   634 => "1403",
+   635 => "1404",
+   636 => "1501",
+   638 => "1502",
+   639 => "1503",
+   637 => "1504",
+  }
+end
+
+data = Submission.where(assignment_id: ids.keys).includes(:lines).map do |submission|
+  { user: submission.user_id, code: ids[submission.assignment_id], time: submission.created_at, status: submission.status, line_count: submission.lines.length }
+end
+
+puts JSON[data]
 
