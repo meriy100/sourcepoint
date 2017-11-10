73ac32aa98b714b1282c1834de10452cf2836ba5
meriy100 <ttattataa@gmail.com>
2017-11-10 18:46:00 +0900

    これでいいかな?

--- app/models/current_assignment.rb
+++ app/models/current_assignment.rb
@@ -5,6 +5,26 @@ class CurrentAssignment < ActiveHash::Base
       code: '06A2',
       assignment_ids: [29, 94, 170, 235, 302, 374, 448],
     }, {
+      id: 595,
+      code: '06B1',
+      assignment_ids: [30, 95, 171, 236, 303, 375, 449],
+    }, {
+      id: 600,
+      code: '07A1',
+      assignment_ids: [101, 242, 308, 380, 454],
+    }, {
+      id: 573,
+      code: '02B1',
+      assignment_ids: [74, 147, 215, 282, 354, 427],
+    }, {
+      id: 623,
+      code: '12A1',
+      assignment_ids: [332, 404, 477],
+    }, {
+      id: 632,
+      code: '1401',
+      assignment_ids: [340, 413, 486],
+    }, {
       id: 574,
       code: '',
       assignment_ids: [75, 148, 216, 283, 355, 428],
@@ -18,7 +38,7 @@ class CurrentAssignment < ActiveHash::Base
       assignment_ids: [34, 104, 175],
     }, {
       id: 441,
-      code: '',
+      code: '05A1',
       assignment_ids: [22, 87, 163, 228, 295, 367],
     },
   ]
