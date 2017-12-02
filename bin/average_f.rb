112a55ed9be526844edf834fabd42cfc9374a7e9
meriy100 <ttattataa@gmail.com>
2017-12-02 16:03:19 +0900

    [Fix] zero divit error rescue

--- bin/average_f.rb
+++ bin/average_f.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'json'
+require 'pry'
 
 
 def calc_print(json)
@@ -8,6 +9,8 @@ def calc_print(json)
   puts "recall : ".concat( (json.map{|d|d["recall"]||0}.inject(:+)/ json.count ).to_s )
   puts "precision : ".concat( (json.map{|d|d["precision"]||0}.inject(:+)/ json.count ).to_s )
   puts "f : ".concat( (json.map{|d|d["f"]||0}.inject(:+)/ json.count ).to_s )
+rescue NoMethodError => e
+  STDERR.puts e
 end
 
 if __FILE__ == $0
