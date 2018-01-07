38af17edc2fa804edb3ce9f9fd6e0a9d2ceab9e4
meriy100 <ttattataa@gmail.com>
2018-01-07 15:59:56 +0900

    data

--- check_all.rb
+++ check_all.rb
@@ -1,14 +1,14 @@
-(
-ARGV ||
-[
-612
-]).each do |id|
-
-  puts id
-  # Attempt.search(created_at_gt: Time.zone.today).result.destroy_all
-  unless system("bin/rake encode:attempts ID=#{id}") && system("bin/rake all_checker:run ID=#{id}")
-    puts "error : #{id}"
-    exit
+def main(ids = [])
+  ids.each do |id|
+    puts id
+    # Attempt.search(created_at_gt: Time.zone.today).result.destroy_all
+    unless system("bin/rake encode:attempts ID=#{id}") && system("bin/rake all_checker:run ID=#{id}")
+      puts "error : #{id}"
+      exit
+    end
   end
 end
 
+if __FILE__ == $0
+  main(ARGV)
+end
--- ex.rb
+++ ex.rb
@@ -1,21 +1,5 @@
-# [587, 595, 594, 574].each do |id|
-#   binding.pry unless system("bin/rake encode:attempts ID=#{id}")
-# end
-
-# attempts_sliced = Attempt.where(current_assignment_id: [587, 595, 594, 574]).pluck(:id).each_slice(5).to_a
 LENGTH = 90
 START_TIME = Time.zone.now
-# attempts_sliced = Attempt.where(current_assignment_id: [595]).pluck(:id).each_slice(5).to_a
-# Tempfile.create('sourcepoint-') do |tmp|
-#   puts "tail -f #{tmp.path}"
-#   max = attempts_sliced.count * 1.0
-#   Parallel.each_with_index(attempts_sliced, in_processes: 2) do |ids, idx|
-#     File.open(tmp.path, "a") do |f|
-#     f.print "\r".concat("#" * (idx * LENGTH / max) .to_i).concat(" " * (LENGTH - idx * LENGTH / max).to_i).concat("|(#{idx}/#{max})")
-#     end
-#     binding.pry unless system("bin/rails r 'Attempt.where(id: #{ids}).each do |a| GC.start; SubmissionCreate.new(a.to_submission, same_search: false).pre_run end'")
-#   end
-# end
 
 ARGV.map(&:to_i).each do |assignment_id|
   attempts = Attempt.where(current_assignment_id: assignment_id)
@@ -46,5 +30,4 @@ ARGV.map(&:to_i).each do |assignment_id|
   CurrentAssignment.find(assignment_id).attempts.sort_by(&:encode_code).each_cons(2) do |first, second|
     first.destroy if first.encode_code == second.encode_code
   end
-  system('bin/rails r check_all.rb')
 end
