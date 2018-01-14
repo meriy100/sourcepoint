cac5f7a9bd123800d9fa305bd54442f73fa18785
meriy100 <ttattataa@gmail.com>
2018-01-14 15:41:13 +0900

    wip

---
+++ dir_map.rb
@@ -0,0 +1,13 @@
+def dirmap(current_assignment_id)
+  comb = CurrentAssignment.find(current_assignment_id).attempts.to_a.uniq{|a|a.encode_code}.combination(2).to_a
+  Parallel.map_with_index(comb, in_processes: 8) do |pair, idx|
+    STDERR.print "\r#{(idx.to_f / comb.count) * 100}" if idx.present?
+    puts Levenshtein.normalized_distance(pair.first.encode_code, pair.second.encode_code)
+  end
+end
+
+if __FILE__ == $0
+  ARGV.each do |arg|
+    dirmap(arg)
+  end
+end
